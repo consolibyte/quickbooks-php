af2f1a3e541ba9164e694b69169f4ba6994de96a
caspian <caspian>
2012-08-09 23:20:31 +0000

    Add support for pagination to the IPP Employee and Job classes.

--- QuickBooks/IPP/Service/Employee.php
+++ QuickBooks/IPP/Service/Employee.php
@@ -21,10 +21,9 @@ QuickBooks_Loader::load('/QuickBooks/IPP/Service.php');
 
 class QuickBooks_IPP_Service_Employee extends QuickBooks_IPP_Service
 {
-	public function findAll($Context, $realmID)
+	public function findAll($Context, $realmID, $query = null, $page = 1, $size = 50)
 	{
-		$xml = null;
-		return parent::_findAll($Context, $realmID, QuickBooks_IPP_IDS::RESOURCE_EMPLOYEE, $xml);
+		return parent::_findAll($Context, $realmID, QuickBooks_IPP_IDS::RESOURCE_EMPLOYEE, $query, null, $page, $size);
 	}
 	
 	/**
--- QuickBooks/IPP/Service/Job.php
+++ QuickBooks/IPP/Service/Job.php
@@ -21,9 +21,8 @@ QuickBooks_Loader::load('/QuickBooks/IPP/Service.php');
 
 class QuickBooks_IPP_Service_Job extends QuickBooks_IPP_Service
 {
-	public function findAll($Context, $realmID)
+	public function findAll($Context, $realmID, $query = null, $page = 1, $size = 50)
 	{
-		$xml = null;
-		return parent::_findAll($Context, $realmID, QuickBooks_IPP_IDS::RESOURCE_JOB, $xml);
+		return parent::_findAll($Context, $realmID, QuickBooks_IPP_IDS::RESOURCE_JOB, $query, null, $page, $size);
 	}
 }
