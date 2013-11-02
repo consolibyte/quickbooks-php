cfdc42c9424e624a6a89c37c128698b472f1696f
Keith Palmer Jr <kpalmer@Keiths-MacBook-Pro.local>
2013-11-02 09:05:21 -0400

    Bug fix for IntuitAnywhere->disconnect() method.

--- QuickBooks/IPP/IntuitAnywhere.php
+++ QuickBooks/IPP/IntuitAnywhere.php
@@ -214,7 +214,9 @@ class QuickBooks_IPP_IntuitAnywhere
 			// Extract the error code
 			$code = (int) QuickBooks_XML::extractTagContents('ErrorCode', $retr);
 			
-			if ($code == 0 or $force)
+			if ($code == 0 or 
+				$code == 270 or 	// Sometimes it returns "270: OAuth Token rejected" for some reason?
+				$force)
 			{
 				return $this->_driver->oauthAccessDelete($arr['app_username'], $arr['app_tenant']);
 			}
