1f9a50b6d9e0803b684716315e2e51250838acdb
Keith Palmer Jr <kpalmer@Keiths-MacBook-Pro.local>
2013-11-03 09:20:37 -0500

    DeliveryInfo IPP v3 class.

---
+++ QuickBooks/IPP/Object/DeliveryInfo.php
@@ -0,0 +1,12 @@
+<?php
+
+QuickBooks_Loader::load('/QuickBooks/IPP/Object.php');
+
+class QuickBooks_IPP_Object_DeliveryInfo extends QuickBooks_IPP_Object
+{	
+	protected function _order()
+	{
+		return array();
+	}
+
+}
