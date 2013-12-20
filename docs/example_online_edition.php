08cb28e8fa48b9f5974dee56d679bd7d8e50591d
Keith Palmer Jr <kpalmer@Keiths-MacBook-Pro.local>
2013-12-20 15:12:55 -0500

    Removing some deprecated stuff.

--- docs/example_online_edition.php
+++ docs/example_online_edition.php
@@ -1,15 +1,14 @@
 <?php
 
 /**
- * Example of connecting to QuickBooks Online edition via qbXML
+ * DO NOT USE THIS FILE ANYMORE!
  * 
- * IMPORTANT: 
- * 	IF YOU ARE BUILDING A SAAS APPLICATION (E.G. YOU ARE ALLOWING MANY OF YOUR 
- * 	CUSTOMERS TO CONNECT THEIR COMPANY FILES TO YOUR WEB APPLICATION) THEN THIS 
- * 	IS PROBABLY *NOT* THE FILE YOU SHOULD LOOKING AT. You should look at the 
- * 	Intuit Partner Platform (and thus the corresponding docs/example_ipp_*.php 
- * 	scripts) in this package.
  * 
+ * All QuickBooks Online implementations should now be using the v3 REST APIs. 
+ * 
+ * 	Quick-start: http://www.consolibyte.com/docs/index.php/PHP_DevKit_for_QuickBooks_-_Intuit_Partner_Platform_Quick-Start
+ * 
+ * 	Code: docs/example_app_ipp_v3/* 
  * 
  * 
  * @author Keith Palmer <keith@ConsoliBYTE.com>
@@ -17,40 +16,3 @@
  * @package QuickBooks
  */
 
-header('Content-Type: text/plain');
-
-require_once dirname(__FILE__) . '/../QuickBooks.php';
-
-// Register in DESKTOP mode to get these. Docs: 
-//	http://www.consolibyte.com/docs/index.php/QuickBooks_Online_via_qbXML#Connecting_with_the_.27Desktop.27_model_of_communication
-$application_id = '134476472';
-$application_login = 'test.www.academickeys.com';
-$connection_ticket = 'TGT-47-1sRm2nXMVfm$n8hb2MZfVQ';
-
-// Create our new gateway instance 
-$Gateway = new QuickBooks_Gateway_OnlineEdition(
-	$application_id,
-	$application_login,
-	$connection_ticket);
-
-$xml = '<QBXMLMsgsRq onError="stopOnError">
-			<VendorAddRq>
-				<VendorAdd>
-					<Name>ConsoliBYTE</Name>
-					<FirstName>Keith</FirstName>	
-					<LastName>Palmer</LastName>	
-					<VendorAddress>
-						<Addr1>123 Test Road</Addr1>	
-						<City>Mt Pleasant</City>	
-						<State>MI</State>	
-						<PostalCode>48858</PostalCode>	
-					</VendorAddress>
-					<Email>support@consolibyte.com</Email>	
-				</VendorAdd>
-			</VendorAddRq>
-		</QBXMLMsgsRq>';
-
-// Send the request
-$resp = $Gateway->qbxml($xml);
-
-print($resp);
