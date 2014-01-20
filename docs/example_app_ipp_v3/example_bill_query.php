b030fe36acafa77b9d5da95de0f41b3f7b65d4d5
Keith Palmer Jr <kpalmer@Keiths-MacBook-Pro.local>
2014-01-20 16:31:06 -0500

    v3 Bill example.

---
+++ docs/example_app_ipp_v3/example_bill_query.php
@@ -0,0 +1,76 @@
+<?php
+
+require_once dirname(__FILE__) . '/config.php';
+
+require_once dirname(__FILE__) . '/views/header.tpl.php';
+
+?>
+
+<pre>
+
+<?php
+
+// Set up the IPP instance
+$IPP = new QuickBooks_IPP($dsn);
+
+// Get our OAuth credentials from the database
+$creds = $IntuitAnywhere->load($the_username, $the_tenant);
+
+// Tell the framework to load some data from the OAuth store
+$IPP->authMode(
+	QuickBooks_IPP::AUTHMODE_OAUTH, 
+	$the_username, 
+	$creds);
+
+// Print the credentials we're using
+//print_r($creds);
+
+// This is our current realm
+$realm = $creds['qb_realm'];
+
+// Load the OAuth information from the database
+if ($Context = $IPP->context())
+{
+	// Set the IPP version to v3 
+	$IPP->version(QuickBooks_IPP_IDS::VERSION_3);
+	
+	$BillService = new QuickBooks_IPP_Service_Bill();
+	
+	$bills = $BillService->query($Context, $realm, "SELECT * FROM Bill ");
+
+	//print_r($customers);
+	
+	foreach ($bills as $Bill)
+	{
+		print('Bill # ' . $Bill->getDocNumber() . ' has a total of $' . $Bill->getTotalAmt() . "\n");
+		
+		$num_line = $Bill->countLine();
+		for ($i = 0; $i < $num_line; $i++)
+		{
+			$Line = $Bill->getLine();
+			print_r($Line);
+		}
+	}
+
+	/*
+	print("\n\n\n\n");
+	print('Request [' . $IPP->lastRequest() . ']');
+	print("\n\n\n\n");
+	print('Response [' . $IPP->lastResponse() . ']');
+	print("\n\n\n\n");
+	*/
+}
+else
+{
+	die('Unable to load a context...?');
+}
+
+?>
+
+</pre>
+
+<?php
+
+require_once dirname(__FILE__) . '/views/footer.tpl.php';
+
+?>
