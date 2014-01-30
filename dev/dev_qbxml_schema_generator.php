badd0f2e607c957bb081dd6faa6f10393bd9e427
Harley Laue <losinggeneration@gmail.com>
2014-01-30 15:40:31 -0600

    Switch back to the SDK 7.0 schema that's included in the repo

--- dev/dev_qbxml_schema_generator.php
+++ dev/dev_qbxml_schema_generator.php
@@ -6,7 +6,7 @@ require_once realpath("../QuickBooks.php");
 
 QuickBooks_Loader::load('/QuickBooks/QBXML/Schema/Generator');
 
-$generator = new QuickBooks_QBXML_Schema_Generator('../data/qbxmlops130.xml');
+$generator = new QuickBooks_QBXML_Schema_Generator('../data/qbxmlops70.xml');
 $dir = __DIR__ . '/tmp_schema';
 
 $generator->saveAll(dirname(__FILE__) . '/tmp/');
