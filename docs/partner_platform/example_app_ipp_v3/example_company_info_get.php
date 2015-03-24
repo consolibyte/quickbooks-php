6c27a79a24f8052f40b606688432258e9487cc8d
Keith Palmer <keith@ConsoliBYTE.com>
2015-03-24 13:24:16 -0400

    Examples.

--- docs/partner_platform/example_app_ipp_v3/example_company_info_get.php
+++ docs/partner_platform/example_app_ipp_v3/example_company_info_get.php
@@ -12,9 +12,39 @@ require_once dirname(__FILE__) . '/views/header.tpl.php';
 
 $CompanyInfoService = new QuickBooks_IPP_Service_CompanyInfo();
 
-$info = $CompanyInfoService->get($Context, $realm);
+$Info = $CompanyInfoService->get($Context, $realm);
 
-print_r($info);
+// Let's get some data! 
+$company_type = $Info->getXPath('//CompanyInfo/NameValue[Name="CompanyType"]/Value');
+print('Company type: ' . $company_type . "\n");
+
+$company_name = $Info->getCompanyName();
+print('Company name (method): ' . $company_name . "\n");
+
+$company_name = $Info->getXPath('//CompanyInfo/CompanyName');
+print('Company name (XPath): ' . $company_name . "\n");
+
+$country = $Info->getCountry();
+print('Country: ' . $country . "\n");
+
+$email = $Info->getEmail()->getAddress();
+print('Address: ' . $email . "\n");
+
+$count = $Info->countNameValue();
+for ($i = 0; $i < $count; $i++)
+{
+	$NameValue = $Info->getNameValue($i);
+	//print_r($NameValue);
+
+	print('NameValue: ' . $NameValue->getName() . ' = ' . $NameValue->getValue() . "\n");
+}
+
+print("\n\n\n");
+
+// Here's a dump of all the data 
+print('Dump of all data: ' . "\n");
+print_r($Info);
+
 
 /*
 print($IPP->lastError($Context));
