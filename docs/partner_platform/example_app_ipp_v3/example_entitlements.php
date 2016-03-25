0db3260c2ffa43ef317c06b73b72c28cf1310488
Keith Palmer <keith@ConsoliBYTE.com>
2016-03-25 17:20:58 -0400

    Support for fetching QBO entitlements.

--- docs/partner_platform/example_app_ipp_v3/example_entitlements.php
+++ docs/partner_platform/example_app_ipp_v3/example_entitlements.php
@@ -6,6 +6,21 @@ require_once dirname(__FILE__) . '/views/header.tpl.php';
 
 ?>
 
+<br>
+<br>
+<br>
+<br>
+
+<h1>
+	DUE TO AN INTUIT BUG, THIS DOES NOT WORK WITH SANDBOX COMPANIES! 
+</h1>
+
+<br>
+<br>
+<br>
+<br>
+<br>
+
 <pre>
 
 <?php
@@ -15,28 +30,31 @@ $EntitlementsService = new QuickBooks_IPP_Service_Entitlements();
 // This gets the entitlements/features of a QBO install 
 $es = $EntitlementsService->entitlements($Context, $realm);
 
-foreach ($es as $e)
+if ($es)
 {
-	print($e->getEntitlementId() . '.: ' . $e->getName() . ' => ' . print_r($e->isOn(), true) . '  (' . $e->getTerm() . ')' . "\n");
+	foreach ($es as $e)
+	{
+		print($e->getEntitlementId() . '.: ' . $e->getName() . ' => ' . print_r($e->isOn(), true) . '  (' . $e->getTerm() . ')' . "\n");
+	}
+
+	print("\n\n\n\n");
+
+	// This gets a bit more information about the QBO install (trial days, plan type, etc.)
+	$is = $EntitlementsService->info($Context, $realm);
+
+	print_r($is);
 }
+else
+{
+	//print_r($es);
+	//print_r($is);
 
-print("\n\n\n\n");
+	print($EntitlementsService->lastRequest($Context));
+	print($EntitlementsService->lastResponse($Context));
 
-// This gets a bit more information about the QBO install (trial days, plan type, etc.)
-$is = $EntitlementsService->info($Context, $realm);
-
-print_r($is);
-
-/*
-print_r($es);
-print_r($is);
-
-print($EntitlementsService->lastRequest($Context));
-print($EntitlementsService->lastResponse($Context));
-
-print('ERROR: ');
-print($EntitlementsService->lastError($Context));
-*/
+	print('ERROR: ');
+	print($EntitlementsService->lastError($Context));
+}
 
 ?>
 
