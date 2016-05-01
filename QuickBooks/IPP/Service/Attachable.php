3a6e25ec13e2411f38d7aa8b251801a206baa312
Ryan Bantz <ryan@rykelabs.com>
2016-05-01 15:56:32 -0400

    Initial support for querying attachments

---
+++ QuickBooks/IPP/Service/Attachable.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ *
+ *
+ * Copyright (c) 2010 Keith Palmer / ConsoliBYTE, LLC.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.opensource.org/licenses/eclipse-1.0.php
+ *
+ * @license LICENSE.txt
+ * @author Ryan Bantz <ryan@rykelabs.com>
+ *
+ * @package QuickBooks
+ * @subpackage IPP
+ */
+
+
+QuickBooks_Loader::load('/QuickBooks/IPP/Service.php');
+
+class QuickBooks_IPP_Service_Attachable extends QuickBooks_IPP_Service
+{
+	public function query($Context, $realm, $query)
+	{
+		return parent::_query($Context, $realm, $query);
+	}
+
+}
