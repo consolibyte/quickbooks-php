b172253e8da14fc62e0757bc011dca1368fd8ac6
Giuseppe Morra <morra850@gmail.com>
2020-04-21 10:03:01 +0200

    added object poemail

---
+++ QuickBooks/IPP/Object/POEmail.php
@@ -0,0 +1,16 @@
+<?php
+
+QuickBooks_Loader::load('/QuickBooks/IPP/Object.php');
+
+class QuickBooks_IPP_Object_POEmail extends QuickBooks_IPP_Object
+{
+	protected function _order()
+	{
+		return array(
+			'Id' => true, 
+			'Address' => true, 
+			'Default' => true, 
+			'Tag' => true,
+			); 
+	}
+}
