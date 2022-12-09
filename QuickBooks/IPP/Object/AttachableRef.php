4d69c0e1e9c28958a349d9503c39ef9fd59ba508
Alex <aik099@users.noreply.github.com>
2022-12-09 10:50:05 +0200

    Don't return "AttachableRef" XML node, when object doesn't have an "EntityType"

--- QuickBooks/IPP/Object/AttachableRef.php
+++ QuickBooks/IPP/Object/AttachableRef.php
@@ -17,16 +17,16 @@ class QuickBooks_IPP_Object_AttachableRef extends QuickBooks_IPP_Object
 	 */
 	public function _asXML_v3($indent, $parent, $optype, $flavor)
 	{
-		$xml = str_repeat("\t", $indent) . '<AttachableRef>' . QUICKBOOKS_CRLF;
-		$invoice_id = str_replace(array('{', '}', '-'), '', $this->getEntityRef());
 		$entity_type = $this->getEntityType();
 
 		if ( !$entity_type ) {
-			// TODO: The "EntityType" field isn't populated anywhere but "create" calls, so fallback to "Invoice".
-			$entity_type = 'Invoice';
+			return '';
 		}
 
-		$xml .= str_repeat("\t", $indent + 1) . '<EntityRef type="' . $entity_type . '">' . $invoice_id . '</EntityRef>';
+		$xml = str_repeat("\t", $indent) . '<AttachableRef>' . QUICKBOOKS_CRLF;
+		$entity_id = str_replace(array('{', '}', '-'), '', $this->getEntityRef());
+
+		$xml .= str_repeat("\t", $indent + 1) . '<EntityRef type="' . $entity_type . '">' . $entity_id . '</EntityRef>';
 		$xml .= QUICKBOOKS_CRLF;
 		$xml .= str_repeat("\t", $indent) . '</AttachableRef>' . QUICKBOOKS_CRLF;
 
