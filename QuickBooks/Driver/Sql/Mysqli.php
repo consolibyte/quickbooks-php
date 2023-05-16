0fc0b505da988c5f7ff01a0c277b1e0947d21294
Roger Dueck <rdueck@mcmillan-mcgee.com>
2023-05-16 14:27:29 -0600

    Enlarge database fields as needed to fit data with mysqli

--- QuickBooks/Driver/Sql/Mysqli.php
+++ QuickBooks/Driver/Sql/Mysqli.php
@@ -30,6 +30,8 @@ QuickBooks_Loader::load('/QuickBooks/Driver.php');
  */
 QuickBooks_Loader::load('/QuickBooks/Driver/Sql.php', false);
 
+QuickBooks_Loader::load('/QuickBooks/Driver/Sql/SqlTable.php', false);
+
 /**
  * QuickBooks utilities class
  */
@@ -189,6 +191,11 @@ class QuickBooks_Driver_Sql_Mysqli extends QuickBooks_Driver_Sql
 	 * Database name
 	 */
 	protected $_dbname;
+
+	/**
+	 * Table objects array
+	 */
+	protected $_tables = [];
 
 	/**
 	 * Create a new MySQLi back-end driver
@@ -360,12 +367,33 @@ class QuickBooks_Driver_Sql_Mysqli extends QuickBooks_Driver_Sql
 			// @todo Should this be implemented...?
 		}
 
-		try {
-			$res = $this->_conn->query($sql);
-		} catch (Exception $e) {
-			error_log("Failed query: $sql");
-			throw $e;
-		}
+		do {
+			$retry = false;
+			try {
+				$res = $this->_conn->query($sql);
+			} catch (Exception $e) {
+				error_log("Failed query: $sql");	// remove after testing what follows
+
+				$errnum = $this->_conn->errno;
+				$errmsg = $this->_conn->error;
+				$location = self::getCaller();
+				error_log("SQL/DB ERROR($errnum)$location: $errmsg [$sql;]");	// move past enlargement check
+				$fieldSizeErrors = [
+					1264	// out of range
+				];
+				if (in_array($errnum,$fieldSizeErrors) || !strncmp($errmsg,'Value out of range',18) ||
+					!strncmp($errmsg,'String exceeds',14) ||
+					!strncmp($errmsg,'Data too long for column',24)
+				) {
+					// enlarge the field to accommodate the value and try again
+					if ($this->enlargeField($sql,$errnum,$errmsg)) {
+						$retry = true;
+						continue;
+					}
+				}
+				throw $e;
+			}
+		} while ($retry);
 
 		$this->_last_error = '';
 		if (!$res)
@@ -378,6 +406,10 @@ class QuickBooks_Driver_Sql_Mysqli extends QuickBooks_Driver_Sql
 
 			trigger_error('Error Num.: ' . $errnum . "\n" . 'Error Msg.:' . $errmsg . "\n" . 'SQL: ' . $sql . "\n" . 'Database: ' . $this->_dbname, E_USER_ERROR);
 			return false;
+		}
+		else {
+			$errnum = 0;
+			$errmsg = '';
 		}
 
 		return $res;
@@ -665,5 +697,114 @@ class QuickBooks_Driver_Sql_Mysqli extends QuickBooks_Driver_Sql
 
 	public function error() {
 		return $this->_last_error;
+	}
+
+	/**
+	 * Enlarge a field that wasn't large enough to hold the data.
+	 */
+	protected function enlargeField($sql, &$errnum, &$errmsg) {
+		// Parse SQL and errmsg to determine table & field
+		// ASSUME: Query is 'INSERT INTO', 'REPLACE INTO', or 'UPDATE'.
+		// ASSUME: Query does not use JOIN, or if it does, enlargement is required on
+		//         the first table listed, and only that table contains that fieldname.
+		$sql = trim($sql);
+		$tokens = " \n\r\t";
+		$a = strtoupper(strtok($sql,$tokens));
+		if ($a == 'UPDATE') $queryType = $a;
+		else {
+			$a = strtoupper(strtok($tokens));
+			if ($a == 'INTO') $queryType = 'INSERT';
+			else {
+				error_log("ERROR: Unexpected query type, doesn't match assumptions: $sql");
+				return false;
+			}
+		}
+
+		// Load table definition
+		$table = trim(strtok($tokens),'`');
+		//error_log("Got table name '$table', queryType='$queryType'");
+		$t = $this->_tables[$table] ?? null;
+		if (!$t) {
+			$this->_tables[$table] = $t = new SqlTable($this,$table);
+		}
+		if (!$t->getSchema()) {
+			error_log("ERROR: Could not load schema for table '$table'");
+			return false;
+		}
+
+		// Parse field name
+		// ASSUME: Field name is the first thing in single quotes, i.e.
+		//           "Data too long for column 'RefNumber'"
+		if (!preg_match("/'([^']+)'/",$errmsg,$m)) {
+			error_log("ERROR: Could not parse field name from error message: $errmsg");
+			return false;
+		}
+		$field = $m[1];
+		//error_log("Got field name '$field'");
+
+		// Check that the field exists
+		$fieldInfo = $t->getFieldDef($field);
+		if (!$fieldInfo) {
+			error_log("ERROR: Field '$field' not found in table '$table'");
+			return false;
+		}
+
+		// Determine what value was attempted
+		if ($queryType == 'INSERT') {
+			// Get list of fields
+			$pos = strpos($sql,$table)+strlen($table)+1;
+			$posA = strpos($sql,'(',$pos)+1;
+			$posB = strpos($sql,')',$pos);
+			$fields = explode(',',substr($sql,$posA,$posB-$posA));
+			$index = array_search($field,$fields);
+
+			// Get list of values
+			// ASSUME: Values come from VALUES, not from a sub-query
+			// ASSUME: Values are never enclosed in double quotes
+			// ASSUME: Only one group of VALUES
+			$pos = strpos(strtoupper($sql),'VALUES',$posB);
+			$posA = strpos($sql,'(',$pos)+1;
+			$valuesStr = preg_replace('/[ \)]+$/','',substr($sql,$posA,-1));
+			$values = str_getcsv($valuesStr,',',"'");
+			$value = $values[$index];
+		}
+		else {
+			// ASSUME: Values are never enclosed in double quotes
+			$pos = strpos($sql,'SET')+strlen('SET')+1;
+			$set = substr($sql,$pos);
+			$value = null;
+			while ($set) {
+				$pos = strpos($set,'=');
+				if ($pos === false) break;
+				$name = trim(substr($set,0,$pos));
+				$set = ltrim(substr($set,$pos+1));
+				$pos = 0;
+				if (substr($set,$pos,1) == "'") {
+					$pos++;
+					$pos2 = strpos($set,"'",$pos) - 1;
+					while ($pos2 > $pos && substr($set,$pos2,1) == '\\') {
+						$pos2++;
+						$pos2 = strpos($set,"'",$pos2+1) - 1;
+						if ($pos2 === false) break 2;
+					}
+					$pos2++;
+				}
+				else {
+					$pos2 = strpos($set,',',$pos);
+					if ($pos2 === false) $pos2 = strlen($set);
+				}
+
+				if ($name == $field) {
+					$value = substr($set,$pos,$pos2-$pos);
+					break;
+				}
+
+				$set = ltrim(substr($set,$pos2+1),',');
+			}
+
+			if ($value === null) return false;
+		}
+
+		return $t->enlargeField($field,$value);
 	}
 }
