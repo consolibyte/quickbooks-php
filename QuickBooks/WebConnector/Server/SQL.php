20d166dce61fdd647a1d998ca76e98d4f963cb9e
Roger Dueck <rdueck@mcmillan-mcgee.com>
2024-01-16 06:42:44 -0700

    Use generic import the missing import actions we need

--- QuickBooks/WebConnector/Server/SQL.php
+++ QuickBooks/WebConnector/Server/SQL.php
@@ -212,10 +212,22 @@ class QuickBooks_WebConnector_Server_SQL extends QuickBooks_WebConnector_Server
 					'QuickBooks_Callbacks_SQL_Callbacks::' . $action . 'Response' );
 			}
 		}
-		$sql_map['CurrencyImport'] = [
-			'QuickBooks_Callbacks_SQL_Callbacks::GenericImportRequest',
-			'QuickBooks_Callbacks_SQL_Callbacks::GenericImportResponse'
+
+		// map actions for generic import
+		$genericImportActions = [
+			'AgingReport', 'BillingRate', 'BillToPay', 'BudgetSummaryReport',
+			'BuildAssembly', 'Currency', 'CustomDetailReport', 'GeneralDetailReport',
+			'GeneralSummaryReport', 'InventorySite', 'ItemAssembliesCanBuild', 'ItemDiscount',
+			'ItemFixedAsset', 'ItemGroup', 'ItemOtherCharge', 'ItemPayment', 'ItemSites',
+			'ItemSubtotal', 'ReceivePaymentToDeposit', 'SalesTaxPayable', 'StandardTerms',
+			'Transaction', 'TransferInventory', 'Transfer'
 		];
+		foreach ($genericImportActions as $name) {
+			$sql_map[$name.'Import'] = [
+				'QuickBooks_Callbacks_SQL_Callbacks::GenericImportRequest',
+				'QuickBooks_Callbacks_SQL_Callbacks::GenericImportResponse'
+			];
+		}
 
 		/*
 		$sql_map[QUICKBOOKS_DERIVE_ITEM] = array(
